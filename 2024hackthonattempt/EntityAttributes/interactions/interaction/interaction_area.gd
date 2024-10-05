209442fe6a89429cf4c2aa881df4bc63280ee653
ryanwmiecz <ryski03@gmail.com>
2024-10-05 11:50:29 -0400

    InteractionStarting

---
+++ 2024hackthonattempt/EntityAttributes/interactions/interaction/interaction_area.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+class_name interactionArea
+
+@export var action_name : String = "Interact"
+
+var interact : Callable = func():
+	pass
--- 2024hackthonattempt/scene_selector.gd
+++ 2024hackthonattempt/scene_selector.gd
@@ -2,7 +2,7 @@
 
 extends Control
 
-
+#Testing #testing im so testing coded
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
