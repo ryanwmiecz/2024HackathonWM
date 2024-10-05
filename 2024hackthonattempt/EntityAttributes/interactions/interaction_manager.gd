a0bd4226d91603970dd8783928eef410039ea3a8
ryanwmiecz <ryski03@gmail.com>
2024-10-05 12:25:35 -0400

    Interactions pretty much set up
    yatta

--- 2024hackthonattempt/EntityAttributes/interactions/interaction_manager.gd
+++ 2024hackthonattempt/EntityAttributes/interactions/interaction_manager.gd
@@ -20,11 +20,25 @@ func _process(delta):
 	if active_areas.size()>0 && can_interaction:
 		active_areas.sort_custom(_sort_by_distance_to_player)
 		label.text = base_text + active_areas[0].action_name
-		label.global_position = active_areas[0].global position
+		label.global_position = active_areas[0].global_position
 		label.global_position.y -= 36
-		
+		label.global_position.x -= label.size.x/2
+		label.show()
+	else:
+		label.hide()
 		
 func _sort_by_distance_to_player(area1, area2):
-	var area1_to_player = player.global_position.distance_to(area1.global_position)
-	var area2_to_player = player.global_position.distance_to(area2.global_position)
-	return area1_to_player < area2_to_player	
+	print("inside sort by distance")
+#	var area1_to_player = player.global_position.distance_to(area1.global_position)
+#	var area2_to_player = player.global_position.distance_to(area2.global_position)
+##	return area1_to_player < area2_to_player	
+
+func _input(event):
+	if event.is_action_pressed("Interaction") && can_interaction:
+		if active_areas.size() > 0:
+			can_interaction = false
+			label.hide()
+			
+			await active_areas[0].interact.call()
+			
+			can_interaction = true
