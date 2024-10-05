9a7f5d102987114cb44cc3396415a7fe2fa56598
DogLoaf0 <aaronwcfl@gmail.com>
2024-10-05 14:20:43 -0400

    Enemy Follows Player

--- 2024hackthonattempt/Entities/EnemyChase.gd
+++ 2024hackthonattempt/Entities/EnemyChase.gd
@@ -2,13 +2,14 @@ extends State
 class_name EnemyChase
 
 var player
-var movespeed = 10.0
+var movespeed = 100.0
 @export var enemy : CharacterBody2D
 
 func Enter():
 	player = get_tree().get_first_node_in_group("Player")
 
 func Physics_Update(delta: float):
+	print(enemy.position.x)
 	if player.position.x < enemy.position.x:
 		enemy.velocity = Vector2(-1 * movespeed, 0)
 	elif player.position.x > enemy.position.x:
