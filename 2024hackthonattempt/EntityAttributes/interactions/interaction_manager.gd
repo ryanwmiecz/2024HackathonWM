86788c2f6ce4553eac5031b3ac19f28431bc0672
ryanwmiecz <ryski03@gmail.com>
2024-10-05 12:14:58 -0400

    InteractionProjectFileAdd
    yeah

--- 2024hackthonattempt/EntityAttributes/interactions/interaction_manager.gd
+++ 2024hackthonattempt/EntityAttributes/interactions/interaction_manager.gd
@@ -16,5 +16,15 @@ func unregister_area(area: interactionArea):
 	if index != 1:
 		active_areas.remove_at(index)
 
-
-	
+func _process(delta):
+	if active_areas.size()>0 && can_interaction:
+		active_areas.sort_custom(_sort_by_distance_to_player)
+		label.text = base_text + active_areas[0].action_name
+		label.global_position = active_areas[0].global position
+		label.global_position.y -= 36
+		
+		
+func _sort_by_distance_to_player(area1, area2):
+	var area1_to_player = player.global_position.distance_to(area1.global_position)
+	var area2_to_player = player.global_position.distance_to(area2.global_position)
+	return area1_to_player < area2_to_player	
