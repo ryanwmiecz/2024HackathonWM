61b5c4a681442c4166a6bc6fc33701468cb6c17c
ryanwmiecz <ryski03@gmail.com>
2024-10-05 22:46:20 -0400

    Main Menu

--- 2024hackthonattempt/scene_selector.gd
+++ 2024hackthonattempt/scene_selector.gd
@@ -5,7 +5,7 @@ var scene = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Levels/RealMainMennu.tscn")
  	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
