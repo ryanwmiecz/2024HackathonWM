d5571fb3d4111a48bce7bd77e68589de52633b00
ryanwmiecz <ryski03@gmail.com>
2024-10-05 23:54:30 -0400

    Pause

---
+++ 2024hackthonattempt/Levels/pause.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pause_or_unpause()
+		
+func pause_or_unpause():
+	if get_tree().paused == true:
+		$"CanvasLayer/VBoxContainer/Button".hide()
+	
+		$"CanvasLayer/VBoxContainer/Button3".hide()
+		get_tree().paused = false
+	elif get_tree().paused == false:
+		$"CanvasLayer/VBoxContainer/Button".show()
+	
+		$"CanvasLayer/VBoxContainer/Button3".show()
+		get_tree().paused = true
+
+
+func _on_button_pressed() -> void:
+	pause_or_unpause()
+
+
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
