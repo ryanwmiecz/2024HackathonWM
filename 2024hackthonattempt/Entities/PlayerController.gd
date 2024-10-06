dd82bdd9e3b27461d04180a2a3dadb0a8862edf1
DogLoaf0 <aaronwcfl@gmail.com>
2024-10-06 00:24:45 -0400

    Updated Coyote TIme

--- 2024hackthonattempt/Entities/PlayerController.gd
+++ 2024hackthonattempt/Entities/PlayerController.gd
@@ -48,9 +48,10 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 	
 	# Handle jump.
-	if ((Input.is_action_just_pressed("ui_accept") || !coyote_timer.is_stopped()) && jump_count > 0) && !dead:
+	if (Input.is_action_just_pressed("ui_accept") && jump_count > 0) && !dead:
 		jump_noise.play()
-		jump_count -= 1
+		if !coyote_timer.is_stopped():
+			jump_count -= 1
 		if is_on_floor() || !coyote_timer.is_stopped():
 			velocity.y = JUMP_VELOCITY
 		else:
