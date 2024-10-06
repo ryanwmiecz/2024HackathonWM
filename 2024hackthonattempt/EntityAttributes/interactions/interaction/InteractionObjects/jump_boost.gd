458f9c092ba41a7d332ef24bbab6331e4b5a06d2
ryanwmiecz <ryski03@gmail.com>
2024-10-05 21:09:52 -0400

    Speed and Jump

--- 2024hackthonattempt/EntityAttributes/interactions/interaction/InteractionObjects/jump_boost.gd
+++ 2024hackthonattempt/EntityAttributes/interactions/interaction/InteractionObjects/jump_boost.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 @onready var interaction_area =$InteractionArea
 @onready var sprite = $Sprite2D
 @onready var player = get_tree().get_first_node_in_group("Player")
+@export var newHeight = -400 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +11,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _command():
-	player.SPEED = 9000
+	player.JUMP_VELOCITY = newHeight
 	print("???")
 	
