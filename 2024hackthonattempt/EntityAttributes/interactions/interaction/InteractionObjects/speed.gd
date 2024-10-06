cbef5e575a11202fae47bad70520f73aec9433b5
DogLoaf0 <aaronwcfl@gmail.com>
2024-10-06 02:12:30 -0400

    Dash Fixed

--- 2024hackthonattempt/EntityAttributes/interactions/interaction/InteractionObjects/speed.gd
+++ 2024hackthonattempt/EntityAttributes/interactions/interaction/InteractionObjects/speed.gd
@@ -11,5 +11,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _command():
 	player.JUMP_VELOCITY = -1
-	player.dash_time = 1
+	player.DASH_VELOCITY = 2000
 	
